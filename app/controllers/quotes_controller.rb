6dd150570df608a119ed85122b237a9fa5c1a2ad
Michael Handkins <michaelhandkins@aol.com>
2020-01-14 23:32:11 +0000

    Created the create action and quote_params in the quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,4 +7,14 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 
+  def create
+    @quote = Quote.create(quote_params)
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+
 end
